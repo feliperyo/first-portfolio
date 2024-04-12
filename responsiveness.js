1cacbe519a651c2b405c1d8eaca5d2e593f44c06
feliperyo <ryofeliperyo@gmail.com>
2024-04-12 13:47:50 -0300

    grande atualização

---
+++ responsiveness.js
@@ -0,0 +1,12 @@
+const swiperEl = document.querySelector('swiper-container');
+
+if (screen.width < 500) {
+    swiperEl.setAttribute('grid-rows', '1');
+    swiperEl.setAttribute('slides-per-view', '1.2');
+
+} else if (screen.width < 1030) {
+    swiperEl.setAttribute('slides-per-view', '1.50');
+
+} else {
+    swiperEl.setAttribute('slides-per-view', '3.35');
+}
