e9ac98bc316362bd7b58ba9fc92e96d33e354e3f
feliperyo <ryofeliperyo@gmail.com>
2024-04-14 10:13:47 -0300

    tentativa btnShowAll

--- scripts.js
+++ scripts.js
@@ -24,4 +24,27 @@ async function showAll() {
     await Promise.all(list.innerHTML = myLi)
 }
 
+async function btnShowAll() {
+    myLi = ''
+
+    for (const project of projects) {
+        myLi += `
+    <swiper-slide>
+                <div class="project">
+                    <a href=${project.href} target="_blank">
+                        <img src=${project.src} class="swiper-img" />
+                        <h3 class="project-h3">${project.name}</h3>
+                        <div class="infos-project">
+                            <p>${project.detail}</p>
+                            <p class="p-reposit">🔗 Ver Repositório no GitHub</p>
+                        </div>
+                    </a>
+                </div>
+            </swiper-slide>
+        `
+
+    }
+    await Promise.all(list.innerHTML = myLi)
+}
+
 showAll()
